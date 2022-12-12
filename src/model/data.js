bd5579d877312f2c602048239966b4aded77597b
jchelsy <59979397+jchelsy@users.noreply.github.com>
2022-12-11 20:21:31 -0700

    Add data model(s) to src/model

---
+++ src/model/data.js
@@ -0,0 +1,226 @@
+export const sliderData = [
+  {
+    title: 'Earth',
+    image: require('../assets/images/planets/earth.png'),
+  },
+  {
+    title: 'The Moon',
+    image: require('../assets/images/planets/moon.png'),
+  },
+  {
+    title: 'Mars',
+    image: require('../assets/images/planets/mars.png'),
+  },
+  {
+    title: 'Venus',
+    image: require('../assets/images/planets/venus.png'),
+  },
+  {
+    title: 'Mercury',
+    image: require('../assets/images/planets/mercury.png'),
+  },
+  {
+    title: 'Jupiter',
+    image: require('../assets/images/planets/jupiter.png'),
+  },
+  {
+    title: 'Saturn',
+    image: require('../assets/images/planets/saturn.png'),
+  },
+  {
+    title: 'Uranus',
+    image: require('../assets/images/planets/uranus.png'),
+  },
+  {
+    title: 'Neptune',
+    image: require('../assets/images/planets/neptune.png'),
+  },
+  {
+    title: 'The Sun',
+    image: require('../assets/images/planets/sun.png'),
+  },
+];
+
+
+export const missionList = [
+  {
+    id: '1',
+    icon: require('../assets/images/missions/geolocation.png'),
+    title: 'Mission 1',
+    difficulty: 'Beginner',
+    isComplete: true,
+    reward: '$5',
+    rewardNum: 5, // 5 TOTAL
+  },
+  {
+    id: '2',
+    icon: require('../assets/images/missions/telescope.png'),
+    title: 'Mission 2',
+    difficulty: 'Beginner',
+    isComplete: true,
+    reward: '$10',
+    rewardNum: 10, // 15 TOTAL
+  },
+  {
+    id: '3',
+    icon: require('../assets/images/missions/space.png'),
+    title: 'Mission 3',
+    difficulty: 'Easy',
+    isComplete: true,
+    reward: '$25',
+    rewardNum: 25, // 40 TOTAL
+  },
+  {
+    id: '4',
+    icon: require('../assets/images/missions/planetarium.png'),
+    title: 'Mission 4',
+    difficulty: 'Easy',
+    isComplete: true,
+    reward: '$40',
+    rewardNum: 40, // 80 TOTAL
+  },
+  {
+    id: '5',
+    icon: require('../assets/images/missions/meteor-shower.png'),
+    title: 'Mission 5',
+    difficulty: 'Easy',
+    isComplete: true,
+    reward: '$70',
+    rewardNum: 70, // 150 TOTAL
+  },
+  {
+    id: '6',
+    icon: require('../assets/images/missions/meteor.png'),
+    title: 'Mission 6',
+    difficulty: 'Easy',
+    isComplete: true,
+    reward: '$100',
+    rewardNum: 100, // 250 TOTAL
+  },
+  {
+    id: '7',
+    icon: require('../assets/images/missions/gravity.png'),
+    title: 'Mission 7',
+    difficulty: 'Easy',
+    isComplete: true,
+    reward: '$150',
+    rewardNum: 150, // 400 TOTAL
+  },
+  {
+    id: '8',
+    icon: require('../assets/images/missions/rocket-ship.png'),
+    title: 'Mission 8',
+    difficulty: 'Intermediate', // TIER-UP
+    isComplete: true,
+    reward: '$400',
+    rewardNum: 400, // 800 TOTAL
+  },
+  {
+    id: '9',
+    icon: require('../assets/images/missions/space-station.png'),
+    title: 'Mission 9',
+    difficulty: 'Intermediate',
+    isComplete: true,
+    reward: '$700',
+    rewardNum: 700, // 1,500 TOTAL
+  },
+  {
+    id: '10',
+    icon: require('../assets/images/missions/cosmonaut.png'),
+    title: 'Mission 10',
+    difficulty: 'Intermediate',
+    isComplete: true,
+    reward: '$1k',
+    rewardNum: 1000, // 2,500 TOTAL
+  },
+  {
+    id: '11',
+    icon: require('../assets/images/missions/tektite.png'),
+    title: 'Mission 11',
+    difficulty: 'Intermediate',
+    isComplete: true,
+    reward: '$1.5k',
+    rewardNum: 1500, // 4,000 TOTAL
+  },
+  {
+    id: '12',
+    icon: require('../assets/images/missions/satellite-dish.png'),
+    title: 'Mission 12',
+    difficulty: 'Hard', // TIER-UP
+    isComplete: true,
+    reward: '$3k',
+    rewardNum: 3000, // 7,000 TOTAL
+  },
+  {
+    id: '13',
+    icon: require('../assets/images/missions/rover.png'),
+    title: 'Mission 13',
+    difficulty: 'Hard',
+    isComplete: true,
+    reward: '$5k',
+    rewardNum: 5000, // 12,000 TOTAL
+  },
+  {
+    id: '14',
+    icon: require('../assets/images/missions/satellite.png'),
+    title: 'Mission 14',
+    difficulty: 'Hard',
+    isComplete: true,
+    reward: '$8k',
+    rewardNum: 8000, // 20,000 TOTAL
+  },
+  {
+    id: '15',
+    icon: require('../assets/images/missions/moon-rover.png'),
+    title: 'Mission 15',
+    difficulty: 'Expert', // TIER-UP
+    isComplete: true,
+    reward: '$15k',
+    rewardNum: 15000, // 35,000 TOTAL
+  },
+  {
+    id: '16',
+    icon: require('../assets/images/missions/solar-system.png'),
+    title: 'Mission 16',
+    difficulty: 'Expert',
+    isComplete: true,
+    reward: '$25k',
+    rewardNum: 25000, // 60,000 TOTAL
+  },
+  {
+    id: '17',
+    icon: require('../assets/images/missions/space-colony.png'),
+    title: 'Mission 17',
+    difficulty: 'Expert',
+    isComplete: true,
+    reward: '$40k',
+    rewardNum: 40000, // 100,000 TOTAL
+  },
+  {
+    id: '18',
+    icon: require('../assets/images/missions/galaxy.png'),
+    title: 'Mission 18',
+    difficulty: 'Master', // TIER-UP
+    isComplete: true,
+    reward: '$100k',
+    rewardNum: 100000, // 200,000 TOTAL
+  },
+  {
+    id: '19',
+    icon: require('../assets/images/missions/blackhole.png'),
+    title: 'Mission 19',
+    difficulty: 'Master',
+    isComplete: true,
+    reward: '$200k',
+    rewardNum: 200000, // 400,000 TOTAL
+  },
+  {
+    id: '20',
+    icon: require('../assets/images/missions/bigbang.png'),
+    title: 'Mission 20',
+    difficulty: 'Legend', // TIER-UP
+    isComplete: false,
+    reward: '$600k',
+    rewardNum: 600000, // 1,000,000 TOTAL
+  },
+];
